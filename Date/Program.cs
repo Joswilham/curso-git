0b97c87cffa1e8d45be3276fedb1b0cfae70fcb4
Joswilham <will.alves.rodrigues@gmail.com>
2019-12-18 21:01:59 -0300

    Projeto iniciado mostrando a mensagem Horas a serem acrescentadas

---
+++ Date/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Date
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Horas a serem acrescentadas:");
+           DateTime n = DateTime.Parse(Console.ReadLine());
+            DateTime d1 = DateTime.Now;
+            DateTime d2 = d1.AddDays('n');
+
+            Console.WriteLine(d1);
+            Console.WriteLine(d2);
+        }
+    }
+}
